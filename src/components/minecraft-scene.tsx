b5e4da4f7a40ccd7fbbfe849192f9251c48a9ab7
hessnt30 <hessnt30@vt.edu>
2025-02-19 21:16:26 -0500

    added vanilla block textures, and have a house working using individual blocks

--- src/components/minecraft-scene.tsx
+++ src/components/minecraft-scene.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useEffect, useRef } from "react";
+import { Suspense } from "react";
 import * as THREE from "three";
 
 export function MinecraftScene() {
   return (
     <Canvas shadows camera={{ position: [10, 10, 10], fov: 25 }}>
-      <Suspense>
+      <Suspense fallback={null}>
         <ambientLight intensity={0.5} />
         <directionalLight
           position={[5, 5, 5]}
@@ -24,55 +24,183 @@ export function MinecraftScene() {
   );
 }
 
-function MinecraftHouse() {
-  const groupRef = useRef<THREE.Group>(null);
-
-  useEffect(() => {
-    if (!groupRef.current) return;
-    groupRef.current.rotation.y = Math.PI / 4;
-  }, []);
+function MinecraftBlock({
+  position,
+  texture,
+}: {
+  position: [number, number, number];
+  texture: string;
+}) {
+  const tex = useTexture(texture);
 
   return (
-    <group ref={groupRef}>
-      {/* Base/Floor */}
-      <mesh receiveShadow position={[0, -0.5, 0]}>
-        <boxGeometry args={[10, 1, 10]} />
-        <meshStandardMaterial color="#5c4433" />
-      </mesh>
-
-      {/* Walls */}
-      {[-4, 4].map((x) => (
-        <mesh key={x} castShadow position={[x, 2, 0]}>
-          <boxGeometry args={[1, 4, 8]} />
-          <meshStandardMaterial color="#c6c6c6" />
-        </mesh>
-      ))}
-      {[-4, 4].map((z) => (
-        <mesh key={z} castShadow position={[0, 2, z]}>
-          <boxGeometry args={[8, 4, 1]} />
-          <meshStandardMaterial color="#c6c6c6" />
-        </mesh>
-      ))}
-
-      {/* Roof */}
-      <mesh castShadow position={[0, 4.5, 0]} rotation={[0, Math.PI / 4, 0]}>
-        <coneGeometry args={[7, 3, 4]} />
-        <meshStandardMaterial color="#8b4513" />
-      </mesh>
-
-      {/* Door */}
-      <mesh castShadow position={[0, 1, 4.1]}>
-        <boxGeometry args={[2, 3, 0.2]} />
-        <meshStandardMaterial color="#8b4513" />
-      </mesh>
-
-      {/* Windows */}
-      {[-2, 2].map((x) => (
-        <mesh key={x} castShadow position={[x, 2, 4.1]}>
-          <boxGeometry args={[1, 1, 0.1]} />
-          <meshStandardMaterial color="#87ceeb" />
-        </mesh>
-      ))}
-    </group>
+    <mesh castShadow receiveShadow position={position}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial map={tex} />
+    </mesh>
   );
+}
+
+function MinecraftSlab({
+  position,
+  texture,
+}: {
+  position: [number, number, number];
+  texture: string;
+}) {
+  const tex = useTexture(texture);
+
+  return (
+    <mesh castShadow receiveShadow position={position}>
+      <boxGeometry args={[1, 0.5, 1]} />
+      <meshStandardMaterial map={tex} />
+    </mesh>
+  );
+}
+
+function MinecraftDoorTop({
+  position,
+  texture,
+}: {
+  position: [number, number, number];
+  texture: string;
+}) {
+  const tex = useTexture(texture);
+
+  return (
+    <mesh castShadow receiveShadow position={position}>
+      <boxGeometry args={[1, 1, 0.25]} />
+      <meshStandardMaterial map={tex} />
+    </mesh>
+  );
+}
+
+function MinecraftDoorBottom({
+  position,
+  texture,
+}: {
+  position: [number, number, number];
+  texture: string;
+}) {
+  const tex = useTexture(texture);
+
+  return (
+    <mesh castShadow receiveShadow position={position}>
+      <boxGeometry args={[1, 1, 0.25]} />
+      <meshStandardMaterial map={tex} />
+    </mesh>
+  );
+}
+
+function MinecraftHouse() {
+  const textures = {
+    wood: "/vanilla/oak_planks.png",
+    stone: "/vanilla/stone.png",
+    cobble_stone: "/vanilla/cobblestone.png",
+    stone_bricks: "/vanilla/stone_bricks.png",
+    glass: "/vanilla/glass.png",
+    door_top: "/vanilla/oak_door_top.png",
+    door_bottom: "/vanilla/oak_door_bottom.png",
+  };
+
+  const blocks = [];
+
+  // Floor
+  for (let x = 0; x < 5; x++) {
+    for (let z = 0; z < 5; z++) {
+      blocks.push(
+        <MinecraftBlock
+          key={`floor-${x}-${z}`}
+          position={[x, 0, z]}
+          texture={textures.stone_bricks}
+        />
+      );
+    }
+  }
+
+  // Walls
+  for (let y = 1; y < 5; y++) {
+    for (let x = 0; x < 5; x++) {
+      // leave hole in wall for door
+      if (x === 2 && (y === 1 || y === 2)) continue;
+      blocks.push(
+        <MinecraftBlock
+          key={`wall-x-${x}-${y}`}
+          position={[x, y, 0]}
+          texture={textures.wood}
+        />
+      );
+      blocks.push(
+        <MinecraftBlock
+          key={`wall-x-${x}-${y}-back`}
+          position={[x, y, 4]}
+          texture={textures.wood}
+        />
+      );
+    }
+    for (let z = 1; z < 4; z++) {
+      blocks.push(
+        <MinecraftBlock
+          key={`wall-z-${z}-${y}`}
+          position={[0, y, z]}
+          texture={textures.wood}
+        />
+      );
+      blocks.push(
+        <MinecraftBlock
+          key={`wall-z-${z}-${y}-right`}
+          position={[4, y, z]}
+          texture={textures.wood}
+        />
+      );
+    }
+  }
+
+  // Roof
+  for (let x = 0; x < 5; x++) {
+    for (let z = 0; z < 5; z++) {
+      blocks.push(
+        <MinecraftSlab
+          key={`roof-${x}-${z}`}
+          position={[x, 5 - 0.25, z]}
+          texture={textures.cobble_stone}
+        />
+      );
+    }
+  }
+
+  // Door
+  blocks.push(
+    <MinecraftDoorTop
+      key="door-top"
+      position={[2, 2, -0.4]}
+      texture={textures.door_top}
+    />
+  );
+
+  blocks.push(
+    <MinecraftDoorBottom
+      key="door-bottom"
+      position={[2, 1, -0.4]}
+      texture={textures.door_bottom}
+    />
+  );
+
+  // Windows
+  blocks.push(
+    <MinecraftBlock
+      key="window-left"
+      position={[1, 2, 0]}
+      texture={textures.glass}
+    />
+  );
+  blocks.push(
+    <MinecraftBlock
+      key="window-right"
+      position={[3, 2, 0]}
+      texture={textures.glass}
+    />
+  );
+
+  return <group>{blocks}</group>;
 }
